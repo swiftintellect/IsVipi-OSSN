193e4a3c4bce802d56b6278224e00eeca5483d56
Jones B <IsVipiOfficial@users.noreply.github.com>
2015-09-28 18:48:06 +0300

    deleted repo

--- admincp/footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-<p class="pull-right" style="float:right"><?php footer_text()?></p>
-</div>
-   <script type="text/javascript" src="<?php echo ISVIPI_STYLE_URL; ?>js/bootstrap.min.js"></script>
-   <script type="text/javascript" src="<?php echo ISVIPI_STYLE_URL; ?>js/alertify.min.js"></script>
-   <?php globalAlerts();?> 
-  </body>
-</html>
--- inc/users/admin_base_header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta http-equiv="content-type" content="text/html;charset=ISO-8859-1" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo $site_title." Admin :".$ACTION.""?></title>
