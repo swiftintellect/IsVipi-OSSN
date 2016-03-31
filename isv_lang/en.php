c0eae0f88a68b75fda34d94eb6b4a9eb0a62ef51
Lauro Vitor <lauro_vitor@hotmail.com>
2016-03-31 05:01:15 -0700

    Added files via upload

---
+++ isv_lang/en.php
@@ -0,0 +1,7 @@
+<?php
+    $translation = array();
+    $translation['welcome_to']							= 'Welcome to <span>%s</span>'; // Site Title
+    $translation['copyright']							= 'Copyright &copy; %s <a href="%s" title="%s">%s</a>. Powered by <a href="//isvipi.org" title="Open Source Social Networking Software" target="_blank">IsVipi OSSN</a>.'; // Year, Site Url, Site Title, Site Title
+    $translation['title_sign_up']						= 'Sign Up!';
+	$translation['button_sign_up']						= 'Sign up';
+?>
